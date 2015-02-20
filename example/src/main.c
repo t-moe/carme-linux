d5b045a7fee6bc7c4da53a1bb95d9da83b545cec
id101010 <aaron@duckpond.ch>
2015-02-20 01:38:48 +0100

    Added st-util stat/stop script and modified the makefile.

--- example/src/main.c
+++ example/src/main.c
@@ -1,83 +1,80 @@
-
-/***
- * Led Blinker
+/*****************************************************************************
+ * Copyright &copy; 2013, Bern University of Applied Sciences.
+ * All rights reserved.
  *
- * Lektion 1 :)
+ * ##### GNU GENERAL PUBLIC LICENSE
  *
- * Lang
- * 16.2.2015
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- */
-
-
-#include <stm32f4xx.h>				/* Processor STM32F407IG				*/
-#include <carme.h>					/* CARME Module							*/
-#include <stdbool.h>
-#include <lcd_lld.h>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ *****************************************************************************/
+#include <stm32f4xx.h>				// Processor STM32F407IG
+#include <carme.h>					// CARME Module
 #include <lcd.h>
+#include <lcd_lld.h>
+#include <color.h>
+#include <stm32f4xx_adc.h>
 
+#define LED 	((volatile unsigned char*)(0x6C000200))
+#define SWITCH 	((volatile unsigned char*)(0x6C000400))
+#define SPEED	500000
 
-volatile unsigned char* LED = (volatile unsigned char*)0x6C000200;
-volatile unsigned char* SWITCH = (volatile unsigned char*)0x6C000400;
+uint16_t colors[12] = {
+	GUI_COLOR_BLACK,
+	GUI_COLOR_WHITE,
+	GUI_COLOR_LIGHT_GRAY,
+	GUI_COLOR_DARK_GREY,
+	GUI_COLOR_RED,
+	GUI_COLOR_YELLOW,
+	GUI_COLOR_ORANGE,
+	GUI_COLOR_BROWN,
+	GUI_COLOR_GREEN,
+	GUI_COLOR_CYAN,
+	GUI_COLOR_BLUE,
+	GUI_COLOR_PINK,
+	GUI_COLOR_MAGENTA
+};
 
-long i;
-unsigned char a;
-bool upwards;
+int main(void)
+{
+	int i = 0, j = 0, m = 0;
+	unsigned char *led = LED;
 
-int main(void) {
-
+	*led = 0b00000001;
 
 	LCD_Init();
+	LCD_Clear(GUI_COLOR_RED);
 
+	while(1){
 
+		for(m = 0; (m <= 12); m++){
 
-	a=1;
-	upwards=true;
+			LCD_Clear(colors[m]);
 
-	for (;;) {
+			for(i = 0; i < 7; i++){
+				*led<<=1;
+				for(j = 0; j < SPEED; j++);
+			}
 
-		if(*SWITCH&0x01 ) {
-			a<<=1;
+			for(i = 7; i > 0; i--){
+				*led>>=1;
+				for(j = 0; j < SPEED; j++);
+			}
 		}
-
-		if(*SWITCH&0x02) {
-			a>>=1;
-		}
-
-		if(a==0) a=1;
-
-		if(*SWITCH&0x04) {
-
-		   if(upwards) {
-			   a<<=1;
-			   if(a==0x80) upwards = false;
-		   }
-		   else  {
-			   a>>=1;
-			   if(a==0x01)upwards = true;
-		   }
-
-		}
-
-		*LED=a;
-
-		if(upwards){
-			LCD_Clear(GUI_COLOR_YELLOW);
-
-		} else {
-			LCD_Clear(GUI_COLOR_BROWN);
-
-		}
-		LCD_DrawCircleF(50,50,15,GUI_COLOR_RED);
-
-
-		for( i=0; i<1000000; i++);
-
-
-
 	}
 
-	return 0U;
+	return 0;
 }
-
 
---
+++ example/utils/st-util-daemon.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+#   Util script which takes care of the starting and stopping of st-util.
+#
+
+pid=/tmp/st-util.pid
+
+case "$1" in
+    "start")
+        if [ -e ${pid} ]; then
+            echo -e "[\033[32m:)\033[m] st-util already running."
+            exit 0
+        fi
+        echo -e "[\033[32m:)\033[m] Starting st-util."
+        /bin/st-util > /dev/null 2>&1 &
+        echo $! > /tmp/st-util.pid
+    ;;
+    "stop")
+        if [ ! -e ${pid} ]; then
+            echo -e "[\033[31mError\033[m] st-util not running."
+            exit 1
+        fi
+        echo -e "[\033[31m:(\033[m] Stopping st-util."
+        killall st-util
+        rm -f /tmp/st-util.pid
+    ;;
+    "status")
+        if [ -e ${pid} ]; then
+            echo -e "[Status] \033[32mrunning.\033[m"
+            exit 0
+        else
+            echo -e "[Status] \033[31mstopped.\033[m"
+            exit 1
+        fi
+    ;;
+    *)
+        echo -e "[\033[31mError\033[m] \033[32mUsage: $0 {start|stop|status}\033[m"
+        exit 1
+esac
